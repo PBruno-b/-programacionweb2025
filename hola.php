68e57de16c9f08f23bcb6e9823f53e4cf3c3178b
PBruno-b <pereirabarreirobruno@gmail.com>
2025-03-27 14:41:05 -0300

    Trabajamos con variables y comentarios

--- hola.php
+++ hola.php
@@ -1,1 +1,20 @@
-<?php echo "hola mundo"; ?> <?php echo "hoa"; ?>
+<?php echo "hola mundo"; ?> <?php echo "hoa"; //esto es una prueba 
+$nombre = 'Manolo'; //Texto. Puede ser comillas simples o dobles (String)
+$edad = 31; //Enteros (Integer)
+$altura = 1.72; //Decimales, usando elpunto en lugar de coma(float)
+$mayorEdad = true; //Verdad o falso (boolean)  ?> 
+
+<?php $nombreCompleto = "Juan Perez" //Primer y en caso de tener segundo nombre de la persona. Poner punto y coma ademas de remplazar las comillas simples por las francesas
+
+$Edad=25;//Anotar la edad de la persona. Remover la mayuscula para evadir confunciones
+
+$estAtura=1.75;//Anota la estatura de la persona. Ajustar el nombre de la variable a $estatura 
+
+$numero=10;//El nombre no es mujy especifico debe ser cambiado pór uno que explique mejor el tipo de dato que quiere almacenar
+
+Apellido='Fernandez'//Guarda el apellido de la persona. Arreglar sintax agregando $
+
+pais_de_origen="Uruguay"//Guarda el pais de nacimiento de la persona. Arreglar sintax agregando $ y ;
+
+$salario = $2000//Guarda el salario mensual dde la persona. Incluir ; al final ademas expecificar el tipo de salario que recibe
+?>
