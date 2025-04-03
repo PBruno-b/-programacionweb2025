c100f268bf58d8294b60d6e8766dd7ed1e8f1c82
PBruno-b <pereirabarreirobruno@gmail.com>
2025-04-03 14:15:19 -0300

    Creamos el archivo y definimos las operaciones

---
+++ operadores.php
@@ -0,0 +1,10 @@
+<?php
+// =asignacion
+// += Suma y Asignacion $edad=1; $edad+=1; ahora edad vale 2
+// -=Resta y asignacion le resta el valor indicado a la derecha
+// *=Multiplicacion y asignacion le multiplica el valor indicado a la derecha
+// /=Division y asignacion divide la variable por el valor indicado a la derecha
+// %Modulo y asignacion
+// .=Cognotacion y asignacion le agrega un texto indicado a la derecha
+
+?>
