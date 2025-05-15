c8d32b11db109f03384fe61fd3638edd27bc358e
PBruno-b <pereirabarreirobruno@gmail.com>
2025-05-15 15:40:25 -0300

    estudiamos array

---
+++ test.php
@@ -0,0 +1,10 @@
+<?php
+$semana=[];
+$semana[]='Lunes';
+//si los parentesis estan vacios agregara el valor al ultimo lugar disponible en este caso 0
+//si los parentesis tienen un numero ejemplo [0] significa que remplazara el dato que se alojaba en el lugar 0
+$semana=['Lunes'];
+$semana=[0]= 'Martes';
+$semana[0];
+//te permite acceder o leer a ese lugar
+?>
