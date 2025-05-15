ae967636b07853f7c9649520f13d9a156a7ae62a
PBruno-b <pereirabarreirobruno@gmail.com>
2025-05-15 15:40:05 -0300

    creamos array

--- monedas.php
+++ monedas.php
@@ -1,23 +1,25 @@
 <?php
 if($_SERVER["REQUEST_METHOD"]=="POST"){ 
-$valorDolar=40;
-$valorEuro=43;
-$valorReal=8;
+//$valorDolar=40;
+//$valorEuro=43;
+//$valorReal=8;
 $cantidadDePesos=floatval($_POST["pesos"]);
 $SeleccionMoneda=floatval($_POST["moneda"]);
+$valores=[40,43,8,"Hoola"];
+var_dump($valores);
 switch($SeleccionMoneda){
     case 1:
-        $cantidadDeDolares= $cantidadDePesos/$valorDolar;
+        $cantidadDeDolares= $cantidadDePesos/$valores[0];
 echo "Resultado de la Conversion: ";
 echo "$cantidadDePesos pesos uruguayos equivalen a ".$cantidadDeDolares. " Dólares"; 
 break;
 case 2:
-    $cantidadDeEuros= $cantidadDePesos/$valorEuro;
+    $cantidadDeEuros= $cantidadDePesos/$valores[1];
 echo "Resultado de la Conversion: ";
 echo "$cantidadDePesos pesos uruguayos equivalen a ".$cantidadDeEuros. " Euros"; 
 break;
 case 3:
-    $cantidadDeReal= $cantidadDePesos/$valorReal;
+    $cantidadDeReal= $cantidadDePesos/$valores[2];
 echo "Resultado de la Conversion: ";
 echo "$cantidadDePesos pesos uruguayos equivalen a ".$cantidadDeReal. " Reales"; 
 break;
