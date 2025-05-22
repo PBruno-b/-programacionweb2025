ccf0ae3d4659cc2ba154fdffc09cc8ae0b1599c7
PBruno-b <pereirabarreirobruno@gmail.com>
2025-05-22 14:37:34 -0300

    Varias cositas nuevas

--- test.php
+++ test.php
@@ -4,7 +4,23 @@ $semana[]='Lunes';
 //si los parentesis estan vacios agregara el valor al ultimo lugar disponible en este caso 0
 //si los parentesis tienen un numero ejemplo [0] significa que remplazara el dato que se alojaba en el lugar 0
 $semana=['Lunes'];
-$semana=[0]= 'Martes';
-$semana[0];
+$semana[3]='Martes';
+var_dump($semana);
 //te permite acceder o leer a ese lugar
+print(count($semana));
+?>
+<?php 
+print("<br>");
+$planeta=[];
+$planeta[]='Marte';
+var_dump($planeta);
+$planeta[]='Tierra';
+var_dump($planeta);
+$planeta[]= 'Venus';
+var_dump($planeta);
+
+foreach($planeta as $planetas){
+    echo "<br>".$planetas ;
+}
+
 ?>
