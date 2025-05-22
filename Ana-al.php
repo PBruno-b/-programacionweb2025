d81e7bc71b5084072f3a363c22972887288f36fc
PBruno-b <pereirabarreirobruno@gmail.com>
2025-05-22 14:58:54 -0300

    Agregramos contadores

--- Ana-al.php
+++ Ana-al.php
@@ -1,9 +1,15 @@
 <?php 
+$contLab=0;
+$contDes=0;
 $dias =['Lunes','Martes','Miercoles','Jueves','Viernes','Sabado','Domingo'];
 foreach($dias as $dia){
     if($dia=='Sabado' || $dia=='Domingo'){
-        echo "". $dia . " Dia de descanso"; 
-    }else
-        echo " ". $dia . " Dia de trabajo ";
-}
+        echo "". $dia . " Dia de descanso <br>";
+        $contDes++; 
+    }else {	       
+         echo " ". $dia . " Dia de trabajo <br>";
+        $contLab++;
+    }
+}   
+echo " Dias de descanso $contDes <br>" . "Dias laborales $contLab";
 ?>
