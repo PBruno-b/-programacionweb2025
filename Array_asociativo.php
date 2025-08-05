c15d0c74a9f37446b67a3c221242882a86238ce4
PBruno-b <pereiraberreirobruno@gmail.com>
2025-08-05 17:59:50 -0300

    se expandio el catalogo

--- Array_asociativo.php
+++ Array_asociativo.php
@@ -16,5 +16,17 @@ $videojuego["Estado"]='a la venta';
 
 echo "<br>Hay un stock de: ".$videojuego["Stock"]. " Y su estado de venta es: ".$videojuego["Estado"];
 
+$catalogo=["Titulo" => ['A'=>"The Legend of Zelda", 'B'=>"Super Mario Bros. 3", 'C'=>"Chrono Trigger"],
+'Consola'=>['NES','NES','C'=>'SNES'], 
+'Año'=>[1986,1988,'C'=>1995],
+'Precio'=>[50.00,45.00,'C'=>80.00], 
+'Stock'=>[10,15,'C'=>5],
+'Estado'=>['a la venta', 'a la venta', 'a la venta']
+];
+
+echo"<br>El titulo es: ".$catalogo["Titulo"]['C'];
+echo"<br>Salio en la consola: ".$catalogo['Consola']['C'];
+echo "<br>En el año: ".$catalogo['Año']['C'];
+echo "<br>Su precio es: $".$catalogo['Precio']['C'];
 
 ?>
