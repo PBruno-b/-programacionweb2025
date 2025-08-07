20ae75fe2d128cc7b5502306a4a8aca057c05b80
PBruno-b <pereirabarreirobruno@gmail.com>
2025-08-07 15:39:36 -0300

    realizamos el ejercicio 4 y empezamos a hacer el 5 con la falta de investigacion de como indicarle mas valores al foreach

--- Array_asociativo.php
+++ Array_asociativo.php
@@ -16,17 +16,27 @@ $videojuego["Estado"]='a la venta';
 
 echo "<br>Hay un stock de: ".$videojuego["Stock"]. " Y su estado de venta es: ".$videojuego["Estado"];
 
-$catalogo=["Titulo" => ['A'=>"The Legend of Zelda", 'B'=>"Super Mario Bros. 3", 'C'=>"Chrono Trigger"],
+/*$catalogo=["Titulo" => ['A'=>"The Legend of Zelda", 'B'=>"Super Mario Bros. 3", 'C'=>"Chrono Trigger"],
 'Consola'=>['NES','NES','C'=>'SNES'], 
 'Año'=>[1986,1988,'C'=>1995],
 'Precio'=>[50.00,45.00,'C'=>80.00], 
 'Stock'=>[10,15,'C'=>5],
 'Estado'=>['a la venta', 'a la venta', 'a la venta']
-];
+];*/
 
-echo"<br>El titulo es: ".$catalogo["Titulo"]['C'];
-echo"<br>Salio en la consola: ".$catalogo['Consola']['C'];
-echo "<br>En el año: ".$catalogo['Año']['C'];
-echo "<br>Su precio es: $".$catalogo['Precio']['C'];
+$inventario=['The legend of Zelda'=>['Titulo'=>'The legend of Zelda', 'Consola'=>'NES','Año'=>'1986', 'Precio' => '50.00', 'Stock'=>10, 'Estado'=>'a la venta'],
+'Super Mario' => ['Titulo' => 'Super Mario Bros', 'Consola' => 'NES', 'Año' => '1988', 'Precio' => '45.00', 'Stock' => 15, 'Estado' => 'a la venta'],
+'Chrono' => ['Titulo' => 'Chrono Trigger', 'Consola' => 'SNES', 'Año' => '1995', 'Precio' => '80.00', 'Stock' => 5, 'Estado' => 'a la venta'],];
 
+/*echo"<br>El titulo es: ".$inventario['The legend of Zelda']['Titulo'];
+echo"<br>Salio en la consola: ".$inventario['Super Mario']['Consola'];
+echo"<br>Salio en la consola: ".$inventario['Chrono']['Consola'];*/
+
+foreach($inventario as $producto){
+    foreach($producto as $datos){
+        if($datos=='Titulo'){
+            echo '<br>El titulo del juego es: '.$datos;
+        }
+    }
+}
 ?>
