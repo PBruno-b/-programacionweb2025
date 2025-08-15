6caa817bae5a7ca3a852b586324f1bee0c5bf134
PBruno-b <pereirabarreirobruno@gmail.com>
2025-08-15 16:16:55 -0300

    Finalizamos la estructura condicional y esperamos el testing

--- Veterinaria.php
+++ Veterinaria.php
@@ -8,11 +8,21 @@ if ($_SERVER["REQUEST_METHOD"]=="POST"){
     $Nombre=string($_POST["nom"]);
     $Edad=int($_POST["age"]);
     $Vacunas=int($_POST["vac"]);
+    $Raza=string($_POST["raz"]);
+    $Fecha=int($_POST["date"]);
     if($validacion_texto!=$Nombre){
         if($Edad<=30){
-         //  if($Vacunas<= 3){}
+           if($Vacunas<= 3){
+                $Mascota=['Nombre'=>$Nombre, 'Edad'=>$Edad, 'Raza'=>$Raza, 'Vacunas'=>$Vacunas, 'Fecha'=>$Fecha];
+           }else{
+               echo "$Vacunas es un dato invalido";
+           }
                 
+        }else{
+            echo "$Edad es un dato invalido"
         }
+    }else{
+        echo "$Nombre es un dato invalido"
     }
 }
 ?>
